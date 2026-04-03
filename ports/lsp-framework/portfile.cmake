f0c8078565c8370fd0ce82bd724bd0b90affbf89
tjk144 <tkovach@wisc.edu>
2026-04-03 09:48:40 -0500

    Fixed the SHA for lsp-framework

--- ports/lsp-framework/portfile.cmake
+++ ports/lsp-framework/portfile.cmake
@@ -2,8 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO leon-bckl/lsp-framework
     REF 1.3.0
-    SHA512 3c4cdce6c65d38e23b7bc524d1abf3ffcbc1af02a642365d948a39f4573abcffa6635caabd47f6aa2155c1796e811
-37bcc5a81b2229f2147a865ecb94fbf53ab
+    SHA512 3c4cdce6c65d38e23b7bc524d1abf3ffcbc1af02a642365d948a39f4573abcffa6635caabd47f6aa2155c1796e81137bcc5a81b2229f2147a865ecb94fbf53ab
 )
 
 # Handle specific compiler requirements
